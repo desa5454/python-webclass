6d2210bb591410de0b3c913bac5e89417301916f
desa5454 <127530389+desa5454@users.noreply.github.com>
2025-11-01 15:17:02 +0300

    Add files via upload

---
+++ Fizzbuzz.py
@@ -0,0 +1,10 @@
+number=int(input('enter a number: '))
+
+if(number%3==0 and number%5==0):
+    print('fizzbuzz')
+elif(number%3==0):
+    print('fizz')
+elif(number%5==0):
+    print('buzz')
+else:
+    print('passed')
---
+++ into.py
@@ -0,0 +1,25 @@
+name = 'desa'
+salary=100
+is_completed=True
+
+# returns capitila words
+print(name.capitalize())
+#returns length
+print(len(name))
+
+# its shows if the characters are alphabetic
+print(name.isalpha())
+# capital
+print(name.upper())
+#tells if the string is is all digit
+print(salary.isdigit())
+#tells if its alphanumeric
+print(name.isalnum())
+#change if the char is small case
+print(name.islower())
+
+
+
+
+
+

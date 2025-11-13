a58e9b3a23b6113336f79733d98f8ca1012766d9
desa5454 <desalenggebre888@gmail.com>
2025-11-13 11:30:45 +0300

    ordered

---
+++ day 1/inputs.py
@@ -0,0 +1,12 @@
+num1 =int(input('enter your first number: '))
+num2 =int(input('enter your second number: '))
+
+sum= num1+num2
+print('The sum is: '+str(sum))
+
+diff= num1-num2
+print('The diffrence is: '+str(diff))
+
+multi= num1*num2
+print('The product is: '+str(multi))
+
